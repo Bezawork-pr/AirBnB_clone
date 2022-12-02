c4ac8cb6471eba54bcce6d0d8e2cede4eb2a5170
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-02 03:08:23 -0800

    Upload fixed bug

--- console.py
+++ console.py
@@ -7,6 +7,7 @@ The class uses cmd module
 The command interpretor should implement
 quit and EOF to exit the program
 This class deals with providing custom prompt
+upload fixed bug
 """
 import cmd
 from models.base_model import BaseModel
